0d2ab52d0e87dab999121b50e3b7268311992915
T0bC <59888795+T0bC@users.noreply.github.com>
2025-09-26 16:01:36 +0200

    created a dataclass dof specemin

---
+++ carlquant_frames/specimen_model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 26 15:48:56 2025
+
+@author: meissnerto
+"""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class Specimen:
+    specimen_id: str
+    source: Path
+    images: list[Path]
+    slices: int
+    regions: str
+    status: str
+    date: float
