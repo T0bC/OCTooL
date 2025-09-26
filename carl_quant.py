327817d1e3858c9bcee0f0d59d500333a6180b1a
T0bC <59888795+T0bC@users.noreply.github.com>
2025-09-26 13:59:33 +0200

    base carl quant

---
+++ carl_quant.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 26 13:58:13 2025
+
+@author: meissnerto
+"""
+
+from utils.status_bar import StatusBar
+from utils.error_handler import handle_errors
+
+@handle_errors("carl_quant.addContent")
+def addContent(self, frame):
+    pass
