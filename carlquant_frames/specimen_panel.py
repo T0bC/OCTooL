16f42d2ff677406dd2c35a3eb691804050d9be67
T0bC <59888795+T0bC@users.noreply.github.com>
2025-09-26 16:02:03 +0200

    first functionality when selecting a specemin in the specemin table

--- carlquant_frames/specimen_panel.py
+++ carlquant_frames/specimen_panel.py
@@ -16,7 +16,7 @@ class specimenPanel:
         self.root = context.root
         self.frame = context.get_frame("carl_specimen")
 
-        self.headers = ['SPECIMEN_ID', 'SOURCE', 'DATE', 'STATUS']
+        self.headers = ['SPECIMEN_ID', 'SLICES', 'REGIONS', 'STATE']
         self._setup_sheet()
 
     def _setup_sheet(self):
@@ -32,6 +32,24 @@ class specimenPanel:
             height=180
         )
         self.sheet.enable_bindings("copy", "delete", "single_select")
+        self.sheet.enable_bindings("single_select", "cell_select")
+        self.sheet.extra_bindings("cell_select", func=self.on_row_selected)
         self.sheet.grid(row=0, column=0, sticky="nsew")
         self.frame.grid_rowconfigure(0, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
+
+    def on_row_selected(self, event):
+        row_index = self.sheet.get_currently_selected()[0]
+        specimen_id = self.sheet.get_cell_data(row_index, 0)
+        specimen_data = self.context.specimen_data.get(specimen_id)
+
+        if specimen_data:
+            #viewer_panel = self.context.get_panel("carl_viewer")
+            results_panel = self.context.get_panel("carl_results")
+
+            #viewer_panel.display_images(specimen_data["images"])
+            results_panel.load_results_for(specimen_id)
+
+            specimen_data.status = "displayed"
+
+            self.sheet.set_cell_data(row_index, 3, "displayed")
